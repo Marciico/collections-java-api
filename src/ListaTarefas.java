de1af74d56946f42975120b399a20236a0605c4b
MarcioFelipe02 <marciofelipe02@outlook.com>
2025-07-26 11:42:22 -0300

    Conclusão da Atividade List

--- src/ListaTarefas.java
+++ src/ListaTarefas.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class ListaTarefas {
+    private List<Tarefa> tarefas;
+
+    public ListaTarefas() {
+        this.tarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        Tarefa novaTarefa = new Tarefa(descricao);
+        tarefas.add(novaTarefa);
+    }
+    public void removerTarefa(String descricao) {
+        tarefas.removeIf(tarefa -> tarefa.getDescricao().equals(descricao));
+    }
+    public int obterNumeroTotalTarefas() {
+        return tarefas.size();
+    }
+
+    public Collection<String> obterDescricaoTarefas() {
+        return tarefas.stream()
+                      .map(Tarefa::getDescricao)
+                      .toList();
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas lista = new ListaTarefas();
+        lista.adicionarTarefa("Estudar Java");
+        lista.adicionarTarefa("Fazer compras");
+
+        System.out.println("Total de tarefas: " + lista.obterNumeroTotalTarefas());
+        System.out.println("Descrições das tarefas: " + lista.obterDescricaoTarefas());
+
+        lista.removerTarefa("Estudar Java");
+        System.out.println("Total de tarefas após remoção: " + lista.obterNumeroTotalTarefas());
+    }
+
 }
--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Tarefa {
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "descricao='" + descricao + '\'' +
+                '}';
+    }
 }
